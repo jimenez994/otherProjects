56271f446f9e39cce594d2edd7c668db64977608
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-05-09 22:02:03 -0400

    _

--- DevC/client/src/actions/profileActions.js
+++ DevC/client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from './types';
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
 
 // get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -31,5 +31,11 @@ export const getCurrentProfile = () => dispatch => {
 export const setProfileLoading = () => {
     return{
         type: PROFILE_LOADING
+    }
+}
+// profile clear 
+export const clearCurrentProfile = () => {
+    return{
+        type: CLEAR_CURRENT_PROFILE
     }
 }
